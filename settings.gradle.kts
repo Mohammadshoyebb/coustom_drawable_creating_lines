8987a653d0b7a9733c46fc4a985d68729f95abc9
TIC_TAC_TOE_PRACTICE_2 <Mohammadshoyebb>
2023-11-01 23:55:53 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "coustom drawable creating line"
+include(":app")
+ 
